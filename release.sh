8af4374ece8dea96a78765da603ff87d1339a033
Kyle McKee <mckee.kyle@gmail.com>
2018-02-25 00:17:53 -0500

    Created release script

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+VERSION=$1
+ESCAPED_VERSION=`echo $VERSION | sed 's/\./\\\./g'`
+
+# Make the version looks OK, and we're on a clean git status
+if [ -z "$(git status --porcelain)" ]; then 
+  echo "Git status looks clean, here we go..."
+else 
+	echo "Unable to release - you need to commit first!"
+	exit 1
+fi
+
+# Update nuspec with version.
+sed -i 's/<version>.*<\/version>/<version>'$ESCAPED_VERSION'<\/version>/g' nPageObject.nuspec
+
+# Commit it, tag it, push it.
+git commit -am "Release v"$VERSION
+git tag -a v$VERSION
+git push origin --tags
+
+# Pack it
+nuget pack nPageObject
+
+# Push it to nuget
+nuget push nPageObject.$!.nupkg
+
